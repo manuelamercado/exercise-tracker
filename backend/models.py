82d9ef20d19172919b25f222d5b72d4125181e43
Manuela Mercado <mmercado@intellisys.com.do>
2021-04-06 17:35:41 -0400

    Starting backend setup and first endpoint (#1)
    * Starting backend setup and endpoints

---
+++ backend/models.py
@@ -0,0 +1,111 @@
+import os
+from sqlalchemy.dialects.postgresql import UUID
+from sqlalchemy import (
+  Column, String, Integer, DateTime, ForeignKey, create_engine
+)
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+import json
+import uuid
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app):
+    app.config.from_object('config')
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+'''
+User
+Entity for person that tracks exercises
+'''
+
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    username = Column(String, nullable=False)
+
+    def __init__(self, username):
+        self.username = username
+
+    def format(self):
+        return {
+          '_id': self.id,
+          'username': self.username,
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        # myparent.children.remove(somechild)
+        db.session.delete(self)
+        db.session.commit()
+
+
+'''
+Exercise
+Entity for exercise that tracks users exercises
+'''
+
+
+class Exercise(db.Model):
+    __tablename__ = 'Exercise'
+
+    id = Column(Integer, primary_key=True)
+    description = Column(String, nullable=False)
+    duration = Column(String, nullable=False)
+    exercise_date = Column(DateTime(timezone=True), server_default=func.now(), nullable=True)
+    user_uuid = Column(UUID(as_uuid=True), ForeignKey('User.id'))
+    user = relationship("User", back_populates="exercises")
+
+    def __init__(self, description, duration, exercise_date):
+        self.description = description
+        self.duration = duration
+        self.exercise_date = exercise_date
+
+    def format(self):
+        return {
+          '_id': self.id,
+          'description': self.description,
+          'duration': self.duration,
+          'date': self.exercise_date,
+          'user': self.user
+        }
+    
+    def short(self):
+        return {
+            'description': self.description,
+            'duration': self.duration,
+            'date': self.exercise_date,
+        }
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        # myparent.children.remove(somechild)
+        db.session.delete(self)
+        db.session.commit()
+
+
+User.exercises = relationship("Exercise", order_by=Exercise.id, back_populates="user")
---
+++ frontend/boilerplate-project-exercisetracker-gomix/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const request = require("request");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});
